675403a3f200619fa5077e3e30e4b1a1c56026d8
uts4v <utsavstha033@gmail.com>
2026-06-18 09:10:31 +0545

    snake game complete

--- game.py
+++ game.py
@@ -1,5 +1,6 @@
 import turtle
 import time
+import random
 
 delay = 0.1
 wn = turtle.Screen()
@@ -15,8 +16,18 @@ head.shape('circle')
 head.color('red')
 head.penup()
 head.goto(0,0)
-
 head.direction= 'stop'
+
+food = turtle.Turtle()
+food.speed(0)
+food.shape('square')
+food.color('green')
+food.penup()
+food.goto(0,100)
+
+segments = []
+
+# Snake Food
 def go_up():
     head.direction="up"
 def go_down():
@@ -52,6 +63,30 @@ wn.onkeypress(go_right, 'd')
 # Main game
 while True:
     wn.update()
+    # check for a collision
+    if head.distance(food)< 20: 
+        #Move the food at random spot
+        x=random.randint(-290,290)
+        y=random.randint(-290,290)
+        food.goto(x,y)
+
+        # Add a segment
+        new_segment = turtle.Turtle()
+        new_segment.speed(0)
+        new_segment.shape("circle")
+        new_segment.color("blue")
+        new_segment.penup()
+        segments.append(new_segment)
+
+    for index in range (len(segments)-1,0,-1 ):
+        x= segments[index-1].xcor()
+        y=segments[index-1].ycor()
+        segments[index].goto(x,y)
+
+    if len(segments)>0:
+        x=head.xcor()
+        y=head.ycor()
+        segments[0]. goto(x,y)
     move()
     time.sleep(delay)
 
